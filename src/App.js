0a21693cd7a8c512185480a349cb486ac0db64b9
정은경 <ek.jung@epinet.kr>
2021-01-21 15:35:35 +0900

    CounterContainer 구현

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Test from "./components/Test";
+import CounterContainer from "./containers/CounterContainer";
 
 function App() {
   // return <Test></Test>;
+  return (
+    <div>
+      <CounterContainer />
+    </div>
+  );
 }
 
 export default App;
--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -6,8 +6,27 @@ import { increase, decrease, setDiff } from "../modules/counter";
 const CounterContainer = () => {
   // useSelector는 리덕스 스토어의 상태를 조회하는 Hook
   // state의 값은 store.getState() 함수를 호출했을 때 나타나는 결과물과 동일
-  const
-  return <div></div>;
+  const { number, diff } = useSelector((state) => ({
+    number: state.counter.number,
+    diff: state.counter.diff,
+  }));
+
+  // useDispatch는 리덕스 스토어의 dispatch를 함수에서 사용할 수 있게 해주는 Hook
+  const dispatch = useDispatch();
+  // 각 액션들을 디스패치하는 함수 작성
+  const onIncrease = () => dispatch(increase());
+  const onDecrease = () => dispatch(decrease());
+  const onSetDiff = () => dispatch(setDiff(diff));
+
+  return (
+    <Counter
+      number={number}
+      diff={diff}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+      onSetDiff={onSetDiff}
+    />
+  );
 };
 
 export default CounterContainer;
