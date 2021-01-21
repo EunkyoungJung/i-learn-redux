192c07a3e018f51b1095a7ad8d952ffa41584bee
정은경 <ek.jung@epinet.kr>
2021-01-21 10:56:15 +0900

    리액트 프로젝트에 리덕스 저굥ㅇ

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import rootReducer from "./modules";
 // import "./exercise";
 
@@ -12,7 +13,9 @@ console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
